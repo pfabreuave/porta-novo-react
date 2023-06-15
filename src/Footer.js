dfa2a048295fdee9cd89eb869e137b8f335e80b9
Pedro Abreu <pfabreuave.gmail.com>
2023-06-15 05:49:37 -0300

    se mofica texto

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <div className='footer'>
             <p><a className='footer__enlace_toti' href='https://totidiversidade.com.br' target='_blank' rel='noreferrer'>Aqui puedes buscar</a></p>
-            <p>Copyright by DaniGoogle 2021</p>
+            <p>Copyright by pfabreuave 2021</p>
         </div>
     )
 }
